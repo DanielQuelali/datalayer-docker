4af832dd2706f8c09021b2a6cf89ef0c2eb0c198
Eric Charles <eric@datalayer.io>
2016-02-16 09:24:53 +0100

    Fix missing libraries to run the tutorial, contributed by @btiernay on https://github.com/datalayer/datalayer-zeppelin/issues/6

--- zeppelin-rscala/resources/install-R-packages.R
+++ zeppelin-rscala/resources/install-R-packages.R
@@ -6,3 +6,6 @@ install.packages("data.table", repos = "http://cran.us.r-project.org")
 install.packages("caret", repos = "http://cran.us.r-project.org")
 install.packages("sqldf", repos = "http://cran.us.r-project.org")
 install.packages("wordcloud", repos = "http://cran.us.r-project.org")
+install.packages(c('devtools','mplot', 'googleVis'), repos = 'http://cran.us.r-project.org');
+require(devtools);
+install_github('ramnathv/rCharts')
