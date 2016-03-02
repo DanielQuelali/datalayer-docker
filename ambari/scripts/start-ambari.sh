2e3461f5f420cacbb15822668505f8c8a76da248
echarles <eric@datalayer.io>
2016-03-02 09:44:24 +0100

    port 808 for ambari, port 8081 for spitfire

--- ambari/scripts/start-ambari.sh
+++ ambari/scripts/start-ambari.sh
@@ -68,7 +68,7 @@ echo
 echo -e $GREEN$BOLD"Add 'w.x.y.z ambari.datalayer.io.local' entry in your local host file where w.x.y.z is one of the following IP address."$NOBOLD$NOCOLOR
 ifconfig | awk '/inet addr/{print substr($2,6)}'
 echo
-echo -e $GREEN$BOLD"You can now browse http://ambari.datalayer.io.local:8081. Enjoy..."$NOBOLD$NOCOLOR
+echo -e $GREEN$BOLD"You can now browse http://ambari.datalayer.io.local:8080. Enjoy..."$NOBOLD$NOCOLOR
 echo
 echo -e $BOLD$YELLOW"""To configure Kerberos, download from 'http://www.oracle.com/technetwork/java/javase/downloads/jce8-download-2133166.html' the 'local_policy.jar' and 'US_export_policy.jar' and copy them into '/usr/lib/jvm/java-1.8.0-openjdk.x86_64/jre/lib/security' folder."""$NOBOLD$NOCOLOR
 
